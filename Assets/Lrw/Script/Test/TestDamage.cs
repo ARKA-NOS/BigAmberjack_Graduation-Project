bd982046dfcbf21ef56acc10250330eddf70b57b
riwoo0902 <imliu4342@gmail.com>
2026-09-02 11:41:59 +0900

    fix : HealthModule

---
+++ Assets/Lrw/Script/Test/TestDamage.cs
@@ -0,0 +1,19 @@
+﻿using Lrw.Script._Core;
+using Lrw.Script.Agent.HealthSystem;
+using UnityEngine;
+
+namespace Lrw.Script.Test
+{
+    public class TestDamage : MonoBehaviour
+    {
+        [SerializeField] private HealthModule healthModule;
+        [SerializeField] private float value;
+
+        [ContextMenu("Health Change")]
+        private void OnHealthChanged()
+        {
+            healthModule.CurrentHealth += value;
+            FDebug.Log(healthModule);
+        }
+    }
+}

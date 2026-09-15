4e7551e5862d0954de861e7cccf1088a340dd0aa
riwoo0902 <imliu4342@gmail.com>
2026-09-16 00:49:05 +0900

    feat : Shader

---
+++ Assets/Lrw/Script/Test/StopTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Lrw.Script.Test
+{
+    public class StopTest : MonoBehaviour
+    {
+        [SerializeField] private Key stopKey = Key.E;
+        [SerializeField] private Key playKey = Key.P;
+        private void Update()
+        {
+            if (Keyboard.current[stopKey].wasPressedThisFrame)
+            {
+                Time.timeScale = 0;
+            }
+            if (Keyboard.current[playKey].wasPressedThisFrame)
+            {
+                Time.timeScale = 1;
+            }
+        }
+        
+    }
+}

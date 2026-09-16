ed2a60649295ea8a26e2ab94bf03c7c49c69d048
riwoo0902 <imliu4342@gmail.com>
2026-09-16 23:50:43 +0900

    save

---
+++ Assets/Lrw/Script/_Core/_FSM/AbstractState.cs
@@ -0,0 +1,10 @@
+﻿namespace Lrw.Script._Core._FSM
+{
+    public abstract class AbstractState : IState
+    {
+        public abstract void Enter();
+        public abstract void StateUpdate();
+        public abstract void StateFixedUpdate();
+        public abstract void Exit();
+    }
+}
